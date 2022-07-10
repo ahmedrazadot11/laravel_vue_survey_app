6c6c96ee4b715a3db8473df68a7f2b7eb84a79b2
mack <ahmedrazadot11@yahoo.com>
2022-07-10 09:43:13 +0500

    installation, ui, login and registeration completed

---
+++ vue/src/components/AuthLayout.vue
@@ -0,0 +1,19 @@
+<template>
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+  <div class="max-w-md w-full space-y-8">
+    <router-view></router-view>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
